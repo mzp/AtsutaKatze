4e51e290ebbe784bafaa5f6350d9806112b79d61
mzp <mzpppp@gmail.com>
2012-04-01 06:46:25 +0900

    Ticket用のデータ型定義

---
+++ modules/katzeCore/src/main/scala/Ticket.scala
@@ -0,0 +1,28 @@
+package org.codefirst.katze.core
+
+import sjson.json._
+
+sealed abstract class Status
+case object Open  extends Status
+case object Close extends Status
+
+case class Ticket(
+  uuid    : String,
+  subject : String,
+  status  : Status
+)
+
+object TicketProtocol extends DefaultProtocol {
+  import JsonSerialization._
+
+  implicit val StatusFormat : Format[Status] =
+    wrap("status")( _.toString,
+                   ( _ : String).toLowerCase match {
+                     case "open" => Open
+                     case "close" => Close
+                   })
+
+  implicit val TicketFormat : Format[Ticket] =
+    asProduct3("uuid", "subject", "status")(Ticket.apply _)(Ticket.unapply(_).get)
+}
+
