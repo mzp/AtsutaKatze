37bcd7669e124366a527ff8b24e857b8b9661c83
mzp <mzpppp@gmail.com>
2012-04-01 13:03:57 +0900

    WebUI作成

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,33 @@
 package controllers
 
+import java.io.File
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import org.codefirst.katze.core._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  import play.api.data.Forms._
+
+  def store =
+    new Store(new File(".katze"))
+
+  val ticketForm  = Form {
+    mapping("subject" -> nonEmptyText)(Ticket.make(_, Open))( t => Some(t.subject))
   }
-  
+
+  def index = Action {
+    val tickets = store.current.tickets
+    Ok(views.html.index(tickets))
+  }
+
+  def newTicketForm = Action {
+    Ok(views.html.newTicket())
+  }
+
+  def newTicket = Action { implicit request =>
+    val ticket = ticketForm.bindFromRequest.get
+    store.apply(Patch.make(AddAction(ticket)))
+    Redirect(routes.Application.index)
+  }
 }
